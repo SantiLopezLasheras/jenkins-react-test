54030f904187b2411ae60d3c34279765cf5bc935
santijll <santijll@example.com>
2025-01-12 12:22:43 +0100

    jest 2

--- jenkinsScripts/sendMessage.js
+++ jenkinsScripts/sendMessage.js
@@ -1,4 +1,4 @@
-const TelegramBot = require("node-telegram-bot-api");
+import TelegramBot from "node-telegram-bot-api";
 const chatId = process.argv[3];
 const botToken = "8083564169:AAEq3CdtmK7OmpIrAJS3HQeOmCyyyqdTUiE";
 const bot = new TelegramBot(botToken, { polling: true });
