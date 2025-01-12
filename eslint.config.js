1c66178d74ac6a4a37c865b139b3e29ac1da16e1
santijll <santijll@example.com>
2025-01-12 12:46:31 +0100

    jest 3

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import react from "eslint-plugin-react";
+import jest from "eslint-plugin-jest";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
@@ -12,8 +13,8 @@ export default [
       ecmaVersion: 2020,
       globals: {
         ...globals.browser,
+        ...globals.jest,
         process: "readonly",
-        jest: "readonly",
       },
       parserOptions: {
         ecmaVersion: "latest",
@@ -23,6 +24,7 @@ export default [
     },
     settings: { react: { version: "18.3" } },
     plugins: {
+      jest: jest,
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
